2cc5ed11e7a3f9e3b52dae711adbd7aebf0a2959
wang li <1366197226@qq.com>
2026-03-07 14:41:08 +0800

    feat: 添加多环境配置和构建脚本，并优化Vite构建输出文件名。

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
         manualChunks(id) {
           if (!id.includes("node_modules")) return;
           if (id.includes("@element-plus/icons-vue"))
