33989b873595fde135d2bcdb5579b3e3703f28a2
wang li <1366197226@qq.com>
2026-03-07 14:33:08 +0800

    Analyze bundle splitting needs

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,52 @@
-import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
-import { defineConfig } from 'vite'
-import { viteMockServe } from 'vite-plugin-mock'
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vite";
+import { viteMockServe } from "vite-plugin-mock";
 
 export default defineConfig({
   plugins: [
     vue(),
     viteMockServe({
-      mockPath: 'mock',
+      mockPath: "mock",
       enable: true,
-      logger: true
+      logger: true,
     }),
     Icons({
-      compiler: 'vue3'
-    })
+      compiler: "vue3",
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@use "@/styles/variables.scss" as *;'
-      }
-    }
-  }
-})
+        additionalData: '@use "@/styles/variables.scss" as *;',
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+          if (id.includes("@element-plus/icons-vue"))
+            return "element-plus-icons";
+          if (id.includes("element-plus")) return "element-plus";
+          if (id.includes("@iconify")) return "iconify";
+          if (id.includes("vue-router")) return "vue-vendor";
+          if (id.includes("pinia")) return "vue-vendor";
+          if (id.includes("/vue/")) return "vue-vendor";
+          if (id.includes("/@vue/")) return "vue-vendor";
+          if (id.includes("axios")) return "axios";
+          if (id.includes("nprogress")) return "nprogress";
+
+          return "vendor";
+        },
+      },
+    },
+  },
+});
