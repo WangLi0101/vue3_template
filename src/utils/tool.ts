786ba51c615c592de7ebe587417570060692be6a
王俐 <>
2026-03-11 17:34:23 +0800

    feat: Add utility functions for file and blob downloading.

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -4,3 +4,24 @@
 export function removeAllSpace(str: string): string {
   return str.replace(/\s/g, "");
 }
+
+/**
+ * 文件下载
+ */
+export function downloadFile(url: string, fileName?: string) {
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = fileName || "";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+/**
+ * blob文件下载
+ */
+export function downloadFileByBlob(blob: Blob, fileName: string) {
+  const url = URL.createObjectURL(blob);
+  downloadFile(url, fileName);
+  URL.revokeObjectURL(url);
+}
