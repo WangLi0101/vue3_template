66f4ac77b95a64b3667f8f3c01792022a831fdaa
王俐 <>
2026-03-20 18:34:47 +0800

    Update error pages and bump commander in lockfile

--- src/views/error/ForbiddenPage.vue
+++ src/views/error/ForbiddenPage.vue
@@ -2,58 +2,9 @@
   <div class="flex h-[640px] items-center justify-center">
     <NoAccess />
     <div class="ml-12">
-      <p
-        v-motion
-        class="mb-4 text-4xl font-medium dark:text-white"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 100,
-          },
-        }"
-      >
-        403
-      </p>
-      <p
-        v-motion
-        class="mb-4 text-gray-500"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 300,
-          },
-        }"
-      >
-        抱歉，你无权访问该页面
-      </p>
-      <router-link
-        v-motion
-        to="/"
-        class="home-link"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 500,
-          },
-        }"
-      >
-        返回首页
-      </router-link>
+      <p class="mb-4 text-4xl font-medium dark:text-white">403</p>
+      <p class="mb-4 text-gray-500">抱歉，你无权访问该页面</p>
+      <router-link to="/" class="home-link"> 返回首页 </router-link>
     </div>
   </div>
 </template>
--- src/views/error/NotFoundPage.vue
+++ src/views/error/NotFoundPage.vue
@@ -2,58 +2,9 @@
   <div class="flex h-[640px] items-center justify-center">
     <NoExist />
     <div class="ml-12">
-      <p
-        v-motion
-        class="mb-4 text-4xl font-medium dark:text-white"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 100,
-          },
-        }"
-      >
-        404
-      </p>
-      <p
-        v-motion
-        class="mb-4 text-gray-500"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 300,
-          },
-        }"
-      >
-        抱歉，你访问的页面不存在
-      </p>
-      <router-link
-        v-motion
-        to="/"
-        class="home-link"
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 500,
-          },
-        }"
-      >
-        返回首页
-      </router-link>
+      <p class="mb-4 text-4xl font-medium dark:text-white">404</p>
+      <p class="mb-4 text-gray-500">抱歉，你访问的页面不存在</p>
+      <router-link to="/" class="home-link"> 返回首页 </router-link>
     </div>
   </div>
 </template>
