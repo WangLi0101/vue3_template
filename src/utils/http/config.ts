4f8f8533d115b1ac448e9327b67a925363eb6c68
王俐 <>
2026-03-23 15:36:00 +0800

    feat: Configure MOCK service URL based on the environment.

--- src/utils/http/config.ts
+++ src/utils/http/config.ts
@@ -7,4 +7,17 @@ const SERVICE_URL_MAP = {
 
 export type ServiceName = keyof ServiceUrlMap;
 
+const env = import.meta.env.MODE;
+switch (env) {
+  case "development":
+    SERVICE_URL_MAP.MOCK = "/api";
+    break;
+  case "production":
+    SERVICE_URL_MAP.MOCK = "/api";
+    break;
+  case "staging":
+    SERVICE_URL_MAP.MOCK = "/api";
+    break;
+}
+
 export { SERVICE_URL_MAP };
